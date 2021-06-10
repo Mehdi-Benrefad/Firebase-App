24651d56dafabf89b903702d0894214f6516dd1f
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-10 12:39:15 +0100

    interface

---
+++ FirebaseApp/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  FirebaseApp
+//
+//  Created by Mehdi Benrefad on 10/06/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
