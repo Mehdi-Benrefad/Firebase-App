a9a686a314d243900bae7b6d0f94afc4079d67d2
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-13 16:16:49 +0100

    add person

--- FirebaseApp/AddViewController.swift
+++ FirebaseApp/AddViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class AddViewController: UIViewController {
 
+    let ref = Database.database().reference(withPath: "people")
+    var items :[Person] = []
+    
+    @IBOutlet weak var fname: UITextField!
+    @IBOutlet weak var lname: UITextField!
+    @IBOutlet weak var age: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func addPerson(_ sender: UIButton) {
+        let addPerson = Person(name: fname.text!, lname: lname.text!, age: age.text!)
+        let addPersonRef = self.ref.childByAutoId()
+        addPersonRef.setValue(addPerson.toAnyObject())
+    }
+    
     /*
     // MARK: - Navigation
 
